34f8ad1a6dbe3d7f3b142aaa953d20e7157bb932
delwar-hosen <86364023+delwar-hosen@users.noreply.github.com>
2023-10-02 11:29:23 +0600

    right or wrong ans from the quiz

--- src/components/ShowQuizOPtion/ShowQuizOPtion.jsx
+++ src/components/ShowQuizOPtion/ShowQuizOPtion.jsx
@@ -1,18 +1,42 @@
-const ShowQuizOPtion = ({ questionAndOption }) => {
-    console.log(questionAndOption)
-    const { question, options, correctAnswer } = questionAndOption;
+import { toast } from "react-toastify";
+import { EyeIcon } from '@heroicons/react/24/solid'
 
-    const handleCorrectAns = () => {
-        console.log('clicked')
+
+
+const ShowQuizOPtion = ({ questionAndOption }) => {
+    // console.log(questionAndOption.options)
+    // console.log(questionAndOption)
+    const { question, options, correctAnswer } = questionAndOption;
+    // console.log(options)
+
+
+    // get quiz and clcik the option show right or wrong ans
+    const handleCorrectAns = (option) => {
+        // console.log(option)
+        if (option === correctAnswer) {
+            toast.success('Congrass', { autoClose: 300 })
+        }
+        else {
+            toast.warning('wrong answer', { autoClose: 300 })
+        }
+
+    }
+
+    //  Eye button click and sho the correct ans
+    const handleCorrectAnsShow = () => {
+        toast(correctAnswer, { autoClose: 2000 })
     }
 
     return (
-        <div className='border m-auto p-10 my-6 w-4/5'>
-            <h2>{question}</h2>
+        <div className='border m-auto p-10 my-6 w-2/3'>
+            <div className="flex justify-between">
+                <h2>{question}</h2>
+                <EyeIcon onClick={handleCorrectAnsShow} className="h-6 w-6 text-blue-500" />
+            </div>
             <div className='mt-6'>
                 {
-                    options.map(option => <div key={option.id} onClick={handleCorrectAns}>
-                        <input type="radio" value="Male" name="gender" /> <> {option}</>
+                    options.map(option => <div key={option.id} >
+                        <p onClick={() => handleCorrectAns(option)}><input type="radio" value="Male" name="gender" />{option}</p>
                     </div>)
                 }
             </div>
--- src/components/ShowQuizeDetails/ShowQuizeDetails.jsx
+++ src/components/ShowQuizeDetails/ShowQuizeDetails.jsx
@@ -5,11 +5,11 @@ import ShowQuizOPtion from '../ShowQuizOPtion/ShowQuizOPtion';
 const ShowQuizeDetails = () => {
     const data = useLoaderData();
     const questionAndOptions = data.data.questions;
-    // console.log(data.data.questions)
+    // console.log(questionAndOptions)
     return (
         <div>
-            <h3>Quiz Of {questionAndOptions.name}</h3>
-            <h4>Total Question {questionAndOptions.total}</h4>
+            <h3>Quiz Of {data.data.name}</h3>
+            <h4>Total Question {data.data.total}</h4>
             {
 
                 questionAndOptions.map(questionAndOption => <ShowQuizOPtion
